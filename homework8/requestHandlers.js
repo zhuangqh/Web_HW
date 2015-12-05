d7e7ca99e5c75b3ea9fe34f6637dd3f4fb53d0f5
zhuangqh <zhuangqhc@163.com>
2015-12-05 10:54:22 +0800

    back end is completed

--- homework8/requestHandlers.js
+++ homework8/requestHandlers.js
@@ -6,11 +6,81 @@
 
 var fs = require('fs');
 var path = require('path');
+var jade = require('jade');
 
 function queryToSignIn (pathname, query, response) {
+    var redirectToRegister = function () { // 跳转至注册页面
+        response.writeHead(302, {
+            'Location': '/register'
+        });
+        response.end();
+    };
 
+    if (!query) { // 无查询内容，跳转至注册页面
+        redirectToRegister();
+    } else {
+        fs.readFile("./userList.txt", function (err, data) {
+            if (err) {throw err;}
+
+            var re = new RegExp("\{\"username\":\"" + query.username + "\"[,\":\\w]*\}");
+            var record = re.exec(data.toString());
+
+            if (!record) { // 用户未注册
+                redirectToRegister();
+            } else {
+                jade.renderFile("./views/userInfo.jade", JSON.parse(record[0]), function (err, html) {
+                    if (err) {throw err;}
+                    response.writeHead(200, {'Content-Type': 'text/html'});
+                    response.end(html);
+                });
+            }
+        });
+    }
 }
 
+function addUser(oPostData, response) {
+    var userInfo = [oPostData.username, oPostData.studentId,
+        oPostData.phone, oPostData.mail];
+
+    var duplicate = [];
+    var keys = ["username", "studentId", "phone", "mail"];
+    var map = {
+        'username': '用户名',
+        'studentId': '学号',
+        'phone': '电话',
+        'mail': '邮箱'
+    };
+    fs.readFile("./userList.txt", function (err, data) {
+        if (err) {throw err;}
+
+        userInfo.forEach(function (element, index) {
+            var strToMatch = "\"" + keys[index] + "\":\"" + userInfo[index] + "\"";
+            if (data.toString().indexOf(strToMatch) != -1)
+                duplicate.push(map[keys[index]]);
+        });
+
+        if (!duplicate.length) {
+            fs.appendFile("./userList.txt", JSON.stringify(oPostData), function (err) {
+                if (err) {throw err;}
+            });
+
+            response.writeHead(302, {    //  用户已注册，跳转至详情页
+                'Location': '/?username=' + oPostData.username
+            });
+            response.end();
+        } else {
+            // 显示重复的信息
+            jade.renderFile("./views/registerError.jade", {errorMessage: duplicate}, function (err, html) {
+                if (err) {throw err;}
+                console.log(duplicate);
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.end(html);
+            });
+        }
+    });
+}
+
+// 获取静态css, js文件
 function getStaticFile(pathname, query, response) {
     var ext = path.extname(pathname); // 文件拓展名
     ext = ext.slice(1); // 去掉小点
@@ -36,5 +106,16 @@ function getStaticFile(pathname, query, response) {
     });
 }
 
+// 渲染静态html
+function getStaticPage(filename, query, response) {
+    jade.renderFile('./views/' + filename + '.jade', function (err, html) {
+        if (err) {throw err;}
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.end(html);
+    });
+}
+
 exports.getStaticFile = getStaticFile;
 exports.queryToSignIn = queryToSignIn;
+exports.addUser = addUser;
+exports.getStaticPage = getStaticPage;
--- homework8/router.js
+++ homework8/router.js
@@ -4,19 +4,26 @@
  * @Create on: 2015/12/3
  */
 
-var jade = require('jade');
+var requestHandlers = require("./requestHandlers");
 
-function route (handle, pathname, query, response) {
+var handle = {
+    "/": requestHandlers.queryToSignIn,
+    "/register": requestHandlers.getStaticPage,
+    "/register/addUser": requestHandlers.addUser,
+    "/static/css/form.css": requestHandlers.getStaticFile,
+    "/static/css/details.css": requestHandlers.getStaticFile
+};
+
+function route (pathname, query, response) {
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
         handle[pathname](pathname, query, response);
     } else {
         console.log("No request handler found for " + pathname);
         // otherwise, return to register page
-        jade.renderFile('./views/register.jade', function (err, html) {
-            response.writeHead(200, {"Content-Type": "text/html"});
-            response.end(html);
-        });
+        response.writeHead(404, {'Content-Type': 'text/plain', 'charset':'utf-8'});
+        response.write("本次作业，你应该访问/?username来登陆，或者访问/register来注册");
+        response.end();
     }
 }
 
